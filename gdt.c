74f3f6875754ccb5203a12221115cc693c8dd59c
NickyHariniaina <ferrissushi@gmail.com>
2026-08-03 15:38:18 +0300

    code

--- gdt.c
+++ gdt.c
@@ -1,1 +1,13 @@
+#include <stdint.h>
 
+void create_gdt_entry(uint8_t *target, uint32_t base, uint32_t limit,
+                      uint8_t access, uint8_t flags) {
+  target[0] = limit & 0xFF;
+  target[1] = (limit >> 8) & 0xFF;
+  target[6] = ((flags << 4) & 0xF0) | (limit >> 16) & 0x0F;
+  target[2] = base & 0xFF;
+  target[3] = (base >> 8) & 0xFF;
+  target[4] = (base >> 16) & 0xFF;
+  target[7] = (base >> 24) & 0xFF;
+  target[5] = access;
+}
